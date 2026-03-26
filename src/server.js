5f315d5baf6f8c9a1a8c4391bb049543c544de95
avanishtatat <avanisht.at.at@gmail.com>
2026-03-26 08:48:34 +0530

    Update package.json scripts and refactor server.js for improved database initialization

--- src/server.js
+++ src/server.js
@@ -3,13 +3,12 @@ const {openDB} = require("./config/db")
 const userRouter = require("./routes/userRoutes")
 const app = express(); 
 
-
 app.use(express.json()); 
 app.use("/api/v1", userRouter);
 
 const initializeDBandServer = async () => {
   try{
-    const db = await openDB(); 
+    await openDB(); 
     app.listen(3000, () => {
       console.log('Server Running At http://localhost:3000');
     })
